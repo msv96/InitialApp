d6133584088ad1e969d71329c675d8ee4a51b981
Srinivasan M <msrini1100@gmail.com>
2023-07-19 06:39:32 +0530

    initial commit

---
+++ src/main/java/com/example/InitialApp/InitialAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.InitialApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialAppApplication.class, args);
+	}
+
+}
