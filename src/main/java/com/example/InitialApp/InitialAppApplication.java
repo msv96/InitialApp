e0f572b8055ea3bb8ffa45ce14e684de43e13306
Srinivasan M <msrini1100@gmail.com>
2023-07-19 07:19:03 +0530

    added get method

--- src/main/java/com/example/InitialApp/InitialAppApplication.java
+++ src/main/java/com/example/InitialApp/InitialAppApplication.java
@@ -2,12 +2,19 @@ package com.example.InitialApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class InitialAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InitialAppApplication.class, args);
 	}
 
+	@GetMapping("/new-app")
+	public String hello() {
+		return "Hi all";
+	}
 }
